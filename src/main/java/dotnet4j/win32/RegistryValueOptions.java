14c1428ec4b3ee2cf4dbb0e0c25f552eb09c44c3
Naohide Sano <namachapanda2005@gmail.com>
2024-05-04 03:28:48 +0900

    anti i18n

--- src/main/java/dotnet4j/win32/RegistryValueOptions.java
+++ src/main/java/dotnet4j/win32/RegistryValueOptions.java
@@ -1,10 +1,10 @@
 package dotnet4j.win32;
 
 public enum RegistryValueOptions {
-    /** オプションの動作は指定されていません。 */
+    /** optional behavior is unspecified */
     None,
     /**
-     * 型の値が、埋め込まれた環境変数を展開せずに取得されます。
+     * the value of the type is retrieved without expanding embedded environment variables.
      *
      * @see "F:Microsoft.Win32.RegistryValueKind.ExpandString"
      */
